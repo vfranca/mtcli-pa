81f7ecfd80ce1810a33d18c955870ca260b2ad2c
Valmir França <vfranca3@gmail.com>
2025-02-15 05:44:12 -0300

    Adicionada a opcao --digitos ao comando bo

--- bo.py
+++ bo.py
@@ -14,21 +14,22 @@ if digits == None:
 @click.command()
 @click.argument("final", type=float)
 @click.argument("inicio", type=float)
-def bo(final, inicio):
+@click.option("--digitos", "-d", type=int, default=0)
+def bo(final, inicio, digitos):
     """Calcula pullbacks e projecoes de rompimento"""
     tamanho = final - inicio
-    click.echo("%.{0}f pt".format(digits) % abs(tamanho))
+    click.echo("%.{0}f pt".format(digitos) % abs(tamanho))
     projecao1 = final + tamanho
     projecao2 = final + tamanho * 2
     #    click.echo("%.{0}f 2X".format(digits) % projecao2)
-    click.echo("%.{0}f 1X".format(digits) % projecao1)
-    click.echo("%.{0}f".format(digits) % final)
+    click.echo("%.{0}f 1X".format(digitos) % projecao1)
+    click.echo("%.{0}f".format(digitos) % final)
     niveis = [0.33, 0.50, 0.66]
     pullback = []
     i = 0
     for n in niveis:
         pullback.append(final - tamanho * n)
-        click.echo("%.{0}f %.2f".format(digits) % (pullback[i], n))
+        click.echo("%.{0}f %.2f".format(digitos) % (pullback[i], n))
         i += 1
 
 
--- tests/test_bo.py
+++ tests/test_bo.py
@@ -13,3 +13,10 @@ def test_calcula_rompimento_de_alta():
 def test_calcula_rompimento_de_baixa():
     res = run.invoke(pa, ["bo", "400", "800"])
     assert res.output == "400 pt\n0 1X\n400\n532 0.33\n600 0.50\n664 0.66\n"
+
+
+def test_calcula_rompimento_com_a_opcao_digitos():
+    res = run.invoke(pa, ["bo", "--digitos", "1", "800", "400"])
+    assert (
+        res.output == "400.0 pt\n1200.0 1X\n800.0\n668.0 0.33\n600.0 0.50\n536.0 0.66\n"
+    )
