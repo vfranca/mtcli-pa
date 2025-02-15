882ed0a92227d70b2da57254f0796768faa2b3bf
Valmir França <vfranca3@gmail.com>
2025-02-14 22:38:06 -0300

    Adicionado teste do gap do dia

--- tests/test_gp.py
+++ tests/test_gp.py
@@ -8,3 +8,8 @@ run = CliRunner()
 def test_calcula_do_gap_do_dia():
     res = run.invoke(pa, ["gp", "400", "800"])
     assert res.output == "400\n"
+
+
+def test_calcula_gap_do_dia_com_variaveis_de_ambiente():
+    res = run.invoke(pa, ["gp"])
+    assert res.output == "-400\n"
