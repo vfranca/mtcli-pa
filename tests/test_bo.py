1ce5435f40c3b5f323201bdf7c6bf5c4b29668fb
Valmir França <vfranca3@gmail.com>
2025-02-14 22:46:43 -0300

    Adicionado teste de rompimento de baixa

--- tests/test_bo.py
+++ tests/test_bo.py
@@ -8,3 +8,8 @@ run = CliRunner()
 def test_calcula_rompimento_de_alta():
     res = run.invoke(pa, ["bo", "800", "400"])
     assert res.output == "400 pt\n1200 1X\n800\n668 0.33\n600 0.50\n536 0.66\n"
+
+
+def test_calcula_rompimento_de_baixa():
+    res = run.invoke(pa, ["bo", "400", "800"])
+    assert res.output == "400 pt\n0 1X\n400\n532 0.33\n600 0.50\n664 0.66\n"
