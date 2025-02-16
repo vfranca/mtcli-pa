1209f36dcf4746b653caf0143bd7a82d3dc0c60c
Valmir França <vfranca3@gmail.com>
2025-02-16 07:17:43 -0300

    Adicionada a opcao digitos ao subcomando tr

--- tests/test_tr.py
+++ tests/test_tr.py
@@ -8,3 +8,11 @@ run = CliRunner()
 def test_calcula_uma_lateralidade():
     res = run.invoke(pa, ["tr", "800", "400"])
     assert res.output == "400 pt\n1200 1X\n800\n668 0.33\n536 0.66\n400\n0 1X\n"
+
+
+def test_calcula_uma_lateralidade_com_opcao_digitos():
+    res = run.invoke(pa, ["tr", "--digitos", "1", "800", "400"])
+    assert (
+        res.output
+        == "400.0 pt\n1200.0 1X\n800.0\n668.0 0.33\n536.0 0.66\n400.0\n0.0 1X\n"
+    )
--- tr.py
+++ tr.py
@@ -3,34 +3,29 @@ Calcula projeções e níveis em lateralidade
 """
 
 import click
-import os
-
-
-digits = os.getenv("DIGITOS")
-if digits == None:
-    digits = 0
 
 
 @click.command()
 @click.argument("maxima", type=float)
 @click.argument("minima", type=float)
-def tr(maxima, minima):
+@click.option("--digitos", "-d", type=int, default=0)
+def tr(maxima, minima, digitos):
     """Calcula projecoes e niveis em lateralidade"""
     tamanho = maxima - minima
-    click.echo("%.{0}f pt".format(digits) % abs(tamanho))
+    click.echo("%.{0}f pt".format(digitos) % abs(tamanho))
     projecao_acima = maxima + tamanho
-    click.echo("%.{0}f 1X".format(digits) % projecao_acima)
-    click.echo("%.{0}f".format(digits) % maxima)
+    click.echo("%.{0}f 1X".format(digitos) % projecao_acima)
+    click.echo("%.{0}f".format(digitos) % maxima)
     niveis = [0.33, 0.66]
     nivel = []
     i = 0
     for n in niveis:
         nivel.append(maxima - tamanho * n)
-        click.echo("%.{0}f %.2f".format(digits) % (nivel[i], n))
+        click.echo("%.{0}f %.2f".format(digitos) % (nivel[i], n))
         i += 1
-    click.echo("%.{0}f".format(digits) % minima)
+    click.echo("%.{0}f".format(digitos) % minima)
     projecao_abaixo = minima - tamanho
-    click.echo("%.{0}f 1X".format(digits) % projecao_abaixo)
+    click.echo("%.{0}f 1X".format(digitos) % projecao_abaixo)
 
 
 if __name__ == "__main__":
